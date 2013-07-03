2aa54b0b4f24b14e3e178a22be0dc74372596acc
Diadara <nithin111@gmail.com>
2013-07-04 00:38:30 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+name = "stemmer"
+
+setup(
+    name=name,
+    version="0.2.1",
+    url="http://silpa.org.in/sort",
+    license="LGPL-3.0",
+    description="Malayalam word stemmer",
+    author="Santhosh Thottingal",
+    author_email="santhosh.thottingal@gmail.com",
+    long_description="""This application helps you to stem the words
+    in the given text. Currently supports only Malayalam.
+    Note that this is very experimental and uses a rule based approach.
+
+    """,
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=['setuptools-git'],
+    install_requires=['setuptools', 'silpa_common'],
+    zip_safe=False,
+)
