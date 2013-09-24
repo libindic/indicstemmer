7bd6ae8a67264ad7b145604cd835d16a0a20b6a6
Jishnu Mohan <jishnu7@gmail.com>
2013-09-24 20:46:31 +0530

    removing unwanted imports, variables
    also did some cleanup. not yet pep8 clean

--- indicstemmer/core.py
+++ indicstemmer/core.py
@@ -17,13 +17,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-# If you find any bugs or have any suggestions email: santhosh.thottingal@gmail.com
+# If you find any bugs or have any suggestions email:
+# santhosh.thottingal@gmail.com
 # URL: http://www.smc.org.in
 
-import sys
 import codecs
 import os
-import string
 import normalizer
 
 
@@ -32,17 +31,18 @@ class Stemmer:
     Instantiate class to get the methods
     """
     def __init__(self):
-        self.rules_file = os.path.join(os.path.dirname(__file__), 'stemmer_ml.rules')
+        self.rules_file = os.path.join(os.path.dirname(__file__), \
+            'stemmer_ml.rules')
         self.rulesDict = None
         self.normalizer = normalizer.getInstance()
 
     def stem(self, text):
         """
         :param text: unicode encoded malayalam string
-        :returns: dictionary with words as the key and the stemmer result as the value
-        stems all the words in the given text and returns a dictionary
+        :returns: dictionary with words as the key and the stemmer result
+        as the values. stems all the words in the given text and
+        returns a dictionary
         """
-        result = ""
         text = self.normalizer.normalize(text)
         if self.rulesDict is None:
             self.rulesDict = self.LoadRules()
@@ -61,8 +61,9 @@ class Stemmer:
             while suffix_pos_itr < word_length:
                 suffix = word[suffix_pos_itr:word_length]
                 if suffix in self.rulesDict:
-                    word_stemmed = word[0:suffix_pos_itr] + self.rulesDict[suffix]
-                    break;
+                    word_stemmed = word[0:suffix_pos_itr] + \
+                        self.rulesDict[suffix]
+                    break
                 suffix_pos_itr = suffix_pos_itr+1
             word_iter = word_iter+1
             if(word_stemmed == ""):
@@ -76,7 +77,8 @@ class Stemmer:
         line = []
         line_number = 0
         rule_number = 0
-        rules_file = codecs. open(self.rules_file,encoding='utf-8', errors='ignore')
+        rules_file = codecs.open(self.rules_file, encoding='utf-8', \
+            errors='ignore')
         while 1:
             line_number = line_number +1
             text = unicode( rules_file.readline())
@@ -90,37 +92,40 @@ class Stemmer:
             if(line == ""):
                 continue
             if(len(line.split("=")) != 2):
-                print "[Error] Syntax Error in the Rules. Line number: ",  line_number
+                print "[Error] Syntax Error in the Rules. Line number: ", \
+                    line_number
                 print "Line: "+ text
                 continue
             lhs = line.split("=")[0].strip()
             rhs = line.split("=")[1].strip()
             if(len(rhs)>0):
-                if(lhs[0]=='"'):
-                    lhs=lhs[1:len(lhs)] # if the string is "quoted"
-                if(lhs[len(lhs)-1]=='"'):
-                    lhs=lhs[0:len(lhs)-1] # if the string is "quoted"
+                if(lhs[0] == '"'):
+                    lhs = lhs[1:len(lhs)] # if the string is "quoted"
+                if(lhs[len(lhs)-1] == '"'):
+                    lhs = lhs[0:len(lhs)-1] # if the string is "quoted"
             if(len(rhs)>0):
-                if(rhs[0]=='"'):
-                    rhs=rhs[1:len(rhs)]  # if the string is "quoted"
-                if(rhs[len(rhs)-1]=='"'):
-                    rhs=rhs[0:len(rhs)-1]     # if the string is "quoted"
-            rule_number=rule_number+1
-            rules_dict[lhs]=rhs
+                if(rhs[0] == '"'):
+                    rhs = rhs[1:len(rhs)]  # if the string is "quoted"
+                if(rhs[len(rhs)-1] == '"'):
+                    rhs = rhs[0:len(rhs)-1]     # if the string is "quoted"
+            rule_number = rule_number+1
+            rules_dict[lhs] = rhs
             #print "[", rule_number ,"] " +lhs + " : " +rhs
         #print "Found ",rule_number, " rules."
         return rules_dict
 
     def trim(self,word):
-        punctuations=['~','!','@','#','$','%','^','&','*','(',')','-','+','_','=','{','}','|' ,':',';','<','>','\,','.','?']
-        word=word.strip()
-        index=len(word)-1
-        while index>0:
+        punctuations = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+            '-', '+', '_', '=', '{', '}', '|' , ':', ';', '<', '>', '\,',
+            '.', '?']
+        word = word.strip()
+        index = len(word)-1
+        while index > 0:
             if word[index] in punctuations:
-                word=word[0:index]
+                word = word[0:index]
             else:
                 break
-            index=index-1
+            index = index-1
         return word
 
     def get_module_name(self):
@@ -133,7 +138,7 @@ class Stemmer:
         """
         returns info on the module
         """
-        return     "Malayalam Stemmer(Experimental)"
+        return "Malayalam Stemmer(Experimental)"
 
 def getInstance():
     return Stemmer()
