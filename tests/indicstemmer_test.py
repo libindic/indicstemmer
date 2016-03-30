a95d1d1c66f371982aef55ddc0c25c2a934c74dd
Arushi Dogra <arushidogra1995@gmail.com>
2016-03-31 04:41:09 +0530

    Changes to meet pep8 standards

--- tests/indicstemmer_test.py
+++ tests/indicstemmer_test.py
@@ -11,10 +11,19 @@ class TestIndicStemmer(unittest.TestCase):
         self.instance = getInstance()
 
     def test_stemmer(self):
-        self.assertEqual(u"തുറക്കുക",self.instance.stem(u"തുറക്കുന്ന","ml_IN")[u"തുറക്കുന്ന"])
-        self.assertEqual(u"खा",self.instance.stem(u"खाएंगे","hi_IN")[u"खाएंगे"])
-        self.assertEqual(u"खेल",self.instance.stem(u"खेलेंगे","hi_IN")[u"खेलेंगे"])
-        self.assertEqual(u"समझ",self.instance.stem(u"समझाएँ","hi_IN")[u"समझाएँ"])
+        self.assertEqual(
+            u"തുറക്കുക", self.instance.stem(
+                u"തുറക്കുന്ന", "ml_IN")[u"തുറക്കുന്ന"])
+        self.assertEqual(
+            u"खा", self.instance.stem(
+                u"खाएंगे", "hi_IN")[u"खाएंगे"])
+        self.assertEqual(
+            u"खेल", self.instance.stem(
+                u"खेलेंगे", "hi_IN")[u"खेलेंगे"])
+        self.assertEqual(
+            u"समझ", self.instance.stem(
+                u"समझाएँ", "hi_IN")[u"समझाएँ"])
+
 
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestIndicStemmer)
