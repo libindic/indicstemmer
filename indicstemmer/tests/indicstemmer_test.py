edfeeea04fe7eb9f4a2751e2da1024d947aa5c5c
Arushi Dogra <arushidogra1995@gmail.com>
2016-04-02 06:55:08 +0530

    Update indicstemmer_test.py

--- indicstemmer/tests/indicstemmer_test.py
+++ indicstemmer/tests/indicstemmer_test.py
@@ -16,7 +16,7 @@ class TestIndicStemmer(unittest.TestCase):
                 u"തുറക്കുന്ന", "ml_IN")[u"തുറക്കുന്ന"])
         self.assertEqual(
             u"खा", self.instance.stem(
-                "खाएगे", "hi_IN")[u"खाएंगे"])
+                "खाएंगे", "hi_IN")[u"खाएंगे"]))
         self.assertEqual(
             u"खेल", self.instance.stem(
                 "खेलेंगे", "hi_IN")[u"खेलेंगे"])
